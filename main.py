9614474bc445bbc79ea682fe9706e31057e531f0
Dani <danalitovsky@gmail.com>
2016-05-04 13:33:24 -0500

    add command line

--- main.py
+++ main.py
@@ -3,7 +3,19 @@
 # MAIN PROGRAM FILE
 
 ##########
+import sys
 
+class color:
+   PURPLE = '\033[95m'
+   CYAN = '\033[96m'
+   DARKCYAN = '\033[36m'
+   BLUE = '\033[94m'
+   GREEN = '\033[92m'
+   YELLOW = '\033[93m'
+   RED = '\033[91m'
+   BOLD = '\033[1m'
+   UNDERLINE = '\033[4m'
+   END = '\033[0m'
 
 ## FUNCTION TO QUERY TWITTER API
 # should output a json of Tweets for now, or store them
@@ -25,4 +37,16 @@ if __name__=="__main__":
 
     word = sys.argv[1]
 
-    #take user's word and query Twitter API function
+    # QUERY TWITTER WITH WORD, get list of words back
+    
+    fake_words = ['hilary', 'prez', 'shower']
+    response_dict = {}
+
+    print '''WELCOME TO TWITTER DETECTIVE! \n 
+                    The word you selected to begin your query is:  %s ''' % word
+    for w in fake_words:
+        response = raw_input('Enter 1 if {} is a word associated with your word / topic of interest: '.format(color.BOLD + w + color.END))
+        response_dict[w] = response
+
+    print response_dict
+
